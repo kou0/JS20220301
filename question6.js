423dcf9fe6f15c6a692bcf8d772ae9c6315983ae
kou0 <imakou1116@gmail.com>
2022-03-01 14:39:38 +0900

    長テーブルのうなぎ屋

---
+++ question6.js
@@ -0,0 +1,45 @@
+const main = () => {
+        //一行目取得
+        const nmLine = lines[0].split(' ')
+        const N = Number(nmLine[0])
+        const M = Number(nmLine[1])
+        let chair = Array(N).fill(false)
+        for (i = 0; i < M; i++) {
+            //一行目以外取得
+            const groupLine = lines[i + 1].split(' ')
+            const a = Number(groupLine[0])
+            const b = Number(groupLine[1])
+                //江戸っ子フラグ
+            let isDup = false
+            const prechair = chair.concat()
+            for (j = b; j < a + b; j++) {
+                if (prechair[j - 1]) {
+                    isDup = true
+                    break;
+                }
+                prechair[j - 1] = true
+            }
+            if (!isDup) {
+                chair = prechair.concat();
+            }
+        }
+        console.log(chair.filter((x) => {
+            return x
+        }).length)
+    }
+    /**
+     * 標準入出力のための処理
+     */
+const reader = require('readline')
+console.log("プログラムスタート")
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let lines = []
+rl.on('line', (line) => {
+    lines.push(line)
+})
+rl.on('close', main)

9a14e378f285a7d3de2676cf2d0be785900b501a
kou0 <imakou1116@gmail.com>
2022-03-01 16:04:42 +0900

    じゃんけんの手の出し方

--- question7.js
+++ question7.js
@@ -3,28 +3,34 @@ const main = () => {
         const N = Number(nmLine[0])
         const M = Number(nmLine[1])
         const opHands = lines[1].split('')
+        const G = 0
+        const C = 2
+        const P = 5
+        let gcount = 0
         let ccount = 0
         let pcount = 0
-        let gcount = 0
         let win = 0
         for (hand of opHands) {
-            if (hand === "C") {
-                ccount++
-            } else if (hand === "P") {
-                pcount++
-            } else {
+            if (hand === "G") {
                 gcount++
+            } else if (hand === "C") {
+                ccount++
+            } else {
+                pcount++
             }
         }
-        for (i = pcount; i > 0; i--) {
-            for (j = 0; j <= gcount; j++) {
-                if ((i * 2) + (j * 5) === M) {
-                    win = ccount + i + j
-                    break;
+        //iはパーを出せる数、jはチョキを出せる数
+        //(opHands.length - i - j) がグーを出す数になる
+        for (i = 0; i < opHands.length; i++) {
+            for (j = 0; j < opHands.length - i; j++) {
+                if (i * P + j * C === M) {
+                    //それぞれを最大限勝てるようにする、超えてしまうと勝てる手がなくなる。
+                    const pwin = Math.min(ccount, opHands.length - i - j) + Math.min(pcount, j) + Math.min(gcount, i)
+                    win = Math.max(win, pwin)
                 }
             }
-            console.log(win)
         }
+        console.log(win)
     }
     /**
      * 標準入出力のための処理

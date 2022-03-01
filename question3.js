bf1e4c60ae3e36857f0b049b879bc496ba89a764
kou0 <imakou1116@gmail.com>
2022-03-01 14:03:33 +0900

    一番小さい値

---
+++ question3.js
@@ -0,0 +1,19 @@
+const main = () => {
+        console.log(Math.min(...lines))
+    }
+    /**
+     * 標準入出力のための処理
+     */
+const reader = require('readline')
+console.log("プログラムスタート")
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let lines = []
+rl.on('line', (line) => {
+    lines.push(line)
+})
+rl.on('close', main)

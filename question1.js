2fcf6a60a3347578dc7ae8ba6d514fc0198735dc
kou0 <imakou1116@gmail.com>
2022-03-01 13:51:21 +0900

    掛け算

---
+++ question1.js
@@ -0,0 +1,19 @@
+const main = () => {
+        console.log(lines[0] * lines[1])
+    }
+    /**
+     * 標準入出力のための処理
+     */
+const reader = require('readline')
+console.log("プログラムスタート")
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let lines = []
+rl.on('line', (line) => {
+    lines.push(line)
+})
+rl.on('close', main)

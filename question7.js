039e11f8c3f4dc873a8c124762c7709437c17f11
kou0 <imakou1116@gmail.com>
2022-03-01 14:59:07 +0900

    [WIP]じゃんけんの手の出し方

---
+++ question7.js
@@ -0,0 +1,42 @@
+const main = () => {
+        const nmLine = lines[0].split(' ')
+        const N = Number(nmLine[0])
+        const M = Number(nmLine[1])
+        const opHands = lines[1].split('')
+        let ccount = 0
+        let pcount = 0
+        let gcount = 0
+        for (hand of opHands) {
+            if (hand === "C") {
+                ccount++
+            } else if (hand === "P") {
+                pcount++
+            } else {
+                gcount++
+            }
+        }
+        for (i = pcount; i > 0; i--) {
+            for (j = 0; j < gcount; j++)
+                if ((i * 2) + (j * 5) === M) {
+                    ccount = ccount + i + j
+                    break;
+                }
+        }
+        console.log(ccount)
+    }
+    /**
+     * 標準入出力のための処理
+     */
+const reader = require('readline')
+console.log("プログラムスタート")
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let lines = []
+rl.on('line', (line) => {
+    lines.push(line)
+})
+rl.on('close', main)
